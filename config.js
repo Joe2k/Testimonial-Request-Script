8814db2078629ffc522946a734d867e8cb818f65
Joe2k <jonathansamuel2k@gmail.com>
2022-01-20 12:27:51 +0530

    [UPD] config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	email: 'f20180460@hyderabad.bits-pilani.ac.in',
-	password: 'MtKulv0T',
+	password: 'password@123',
 	note: 'Hey There! I would like you to write a Testimonial for my Yearbook :)',
 };
